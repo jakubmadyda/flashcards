c15fbda7eedd07aab6539ae603a17edf3cf87f1a
Jakub Madyda <jakub.madyda17@gmail.com>
2023-02-15 15:03:29 +0100

    repair finishlearning and add function for capitalize

--- src/components/FinishLearning/FinishLearning.jsx
+++ src/components/FinishLearning/FinishLearning.jsx
@@ -1,54 +1,50 @@
 import {useLocation, useNavigate} from "react-router-dom";
 import {useEffect, useState} from "react";
-import {PieChart, Pie, ResponsiveContainer, Cell, LabelList, Legend, Tooltip} from "recharts";
+import {Cell, LabelList, Legend, Pie, PieChart, ResponsiveContainer, Tooltip} from "recharts";
+import {capitalize} from "../../helpers/helpers_string";
 
-const colors = ['#F47A1F', '#00529B', '#377B2B']
+const colors = ['#EC6B56', '#FFC154', '#47B39C']
+
 
 function FinishLearning() {
-    const [data, setData] = useState([
-        {
-            name: 'Good',
-            value: 7
-        },
-        {
-            name: 'Not Bad',
-            value: 2
-        },
-        {
-            name: 'Very Bad',
-            value: 3
-        }
-    ])
+    const [data, setData] = useState([]);
     const {state} = useLocation();
     const navigate = useNavigate();
 
     useEffect(() => {
         if (state === null) {
-            // navigate('/')
+            navigate('/');
+        } else {
+            setData(Object.entries(state).map((item) => ({
+                name: capitalize(item[0]),
+                value: item[1]
+            })))
         }
-
-        setData()
     }, [navigate, state])
 
 
     return (
         <div className='mt-3'>
             <h1>You are better than you think!</h1>
-            <div style={{'width': '100%', 'height': '100vh'}}>
+            <div style={{'width': '100%', 'height': '50vh'}}>
                 <ResponsiveContainer>
                     <PieChart>
                         <Legend verticalAlign="top" height={36}/>
-                        <Tooltip cursor={{stroke: "red", strokeWidth: 2}}/>
+                        <Tooltip cursor={{stroke: 'red', strokeWidth: 1}}/>
                         <Pie data={data} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={0}
                              fill="#82ca9d">
-                            <LabelList dataKey="value" position="top" />
-                            {data.map((entry, index) => (
-                                <Cell key={`cell-${index}`} fill={colors[index]}/>
-                            ))}
+                            <LabelList dataKey="value" position="top"/>
+                            {
+                                data.map((entry, index) => (
+                                    <Cell key={`cell-${index}`} fill={colors[index]}/>
+                                ))
+                            }
                         </Pie>
+
                     </PieChart>
                 </ResponsiveContainer>
             </div>
+
         </div>
     );
 }
